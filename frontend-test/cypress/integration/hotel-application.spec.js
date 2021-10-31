75e6bba6065b389b36768b3a7ad17a967395edc9
ridafarrukh <ridafarrukh92@gmail.com>
2021-10-31 15:30:53 +0100

    created a file .gitignore and create test suite and test case

---
+++ frontend-test/cypress/integration/hotel-application.spec.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress"/>
+
+// This is a test suite
+describe('Hotel application test suite', function(){
+
+    // This is a test case
+    it('perform valid login' , function(){
+        cy.visit('http://localhost:3000')
+    })
+
+
+
+})
